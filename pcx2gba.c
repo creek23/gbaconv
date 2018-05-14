0dc011451ab4067f41dd2562f9238dfb06d37bb9
creek23 <creek23@users.noreply.github.com>
2018-05-15 00:09:23 +0800

    patterned output to "dovotos pcx->gba program"
    * reduced needed input to "inputfile" only (ex: pcx2gba input.pcx) instead of previous `input.pcx output.inc array_name'
    * auto-used `input' name from `input.pcx' to create 'inputData' and `inputPalette'
    * will auto-use `input.h' as output file from `input.pcx'
    * added some header on output.h
    * added some formatting

--- pcx2gba.c
+++ pcx2gba.c
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 FILE *input_file;
 unsigned char *input_file_buffer;
@@ -63,14 +64,14 @@ int run_position;
 /*****************************************************************************/
 /* Main                                                                      */
 /*****************************************************************************/
-
 int main (int argc, char *argv[]) {
    printf("-------------------------------------------------------------------------------\n");
    printf("      PCX to GBA Converter - GBAconv 1.00 (c) by Frederic Cambus 2002-2017\n");
    printf("-------------------------------------------------------------------------------\n\n");
 
-   if (argc!=4) {
-      printf("USAGE: pcx2gba input.pcx output.inc array_name (Input File must be 8-bpp PCX)\n\n");
+   if (argc!=2) {
+//      printf("USAGE: pcx2gba input.pcx output.inc array_name (Input File must be 8-bpp PCX)\n\n");
+      printf("USAGE: pcx2gba input.pcx");// output.inc array_name (Input File must be 8-bpp PCX)\n\n");
       exit(0);
    }
 
@@ -79,7 +80,7 @@ int main (int argc, char *argv[]) {
 /*****************************************************************************/
 /* Load Input File                                                           */
 /*****************************************************************************/
-
+	
    stat (argv[1], &input_file_stat);
    input_file_size=input_file_stat.st_size;
 
@@ -99,7 +100,16 @@ int main (int argc, char *argv[]) {
    fread(input_file_buffer,input_file_size,1,input_file);
    fclose(input_file);
 
-
+	int len = strlen(argv[1]);
+	
+	char refName[len];
+	
+	memcpy(refName, argv[1], len-4);
+	refName[len] = '\0';
+	
+	char outfile[len-2];
+	strcpy(outfile, refName);
+	strcat(outfile, ".h");	
 
 /*****************************************************************************/
 /* Check that the file is a valid 8-bpp PCX                                  */
@@ -148,44 +158,96 @@ int main (int argc, char *argv[]) {
 /*****************************************************************************/
 /* Create Output File                                                        */
 /*****************************************************************************/
-
-   output_file=fopen(argv[2],"w");
-   if (output_file==NULL) {
-      printf("ERROR: Cannot create file %s\n\n",argv[2]);
-      exit(-1);
-   }
-
-   printf("INPUT  FILE: %s (%ix%ix%i-bpp)\n",argv[1],pcx_header.x_max+1,pcx_header.y_max+1,pcx_header.bits_per_pixel);
-   printf("OUTPUT FILE: %s\n\n",argv[2]);
-
-   fprintf(output_file,"const u16 %s_palette[] = {\n", argv[3]);
-
-   for (loop=0;loop<256;loop++) {
-      fprintf(output_file,"0x%x,",(pcx_image_palette[loop*3] | pcx_image_palette[(loop*3)+1]<<5 | pcx_image_palette[(loop*3)+2]<<10));
-   }
-
-   fseek(output_file,ftell(output_file)-1,0);
-   fprintf(output_file,"};\n\n");
-
-   fprintf(output_file,"const u16 %s[] = {\n", argv[3]);
-
-   for (loop=0;loop<pcx_buffer_size/2;loop++) {
-      fprintf(output_file,"0x%x,",(pcx_buffer[loop*2] | pcx_buffer[(loop*2)+1]<<8));
-   }
-
-   fseek(output_file,ftell(output_file)-1,0);
-   fprintf(output_file,"};\n");
-
-
-
+	
+	output_file = fopen(outfile,"w");
+	if (output_file == NULL) {
+		printf("ERROR: Cannot create file %s\n\n", outfile);
+		exit(-1);
+	}
+	
+	printf("INPUT  FILE: %s (%ix%ix%i-bpp)\n",argv[1],pcx_header.x_max+1,pcx_header.y_max+1,pcx_header.bits_per_pixel);
+	printf("OUTPUT FILE: %s\n\n",outfile);
+	
+	fprintf(output_file, "/*----------------------------------------------------------------------\n");
+	fprintf(output_file, " * PCX to GBA Converter - GBAconv 1.00 (c) by Frederic Cambus 2002-2017 \n");
+	fprintf(output_file, " *                                1.01 (c) quickmod by creek23 2018     \n");
+	fprintf(output_file, " *----------------------------------------------------------------------*/\n");
+	fprintf(output_file, "#define %s_WIDTH   %i\n"    , refName, pcx_header.x_max+1);
+	fprintf(output_file, "#define %s_HEIGHT  %i\n\n\n", refName, pcx_header.y_max+1);
+	
+	fprintf(output_file, "const u16 %sData[] = {\n                    ", refName);
+	int l_ctr = 0;
+	int l_data = 0;
+	int l_datasize = pcx_buffer_size/2;
+	for (loop = 0;loop < l_datasize; ++loop) {
+		l_data = (pcx_buffer[loop*2] | pcx_buffer[(loop*2)+1]<<8);
+		if (l_data == 00) {
+			fprintf(output_file,"0x0000");
+		} else if (l_data < 16) {
+			fprintf(output_file,"0x000%x",l_data);
+		} else if (l_data < 256) {
+			fprintf(output_file,"0x00%x",l_data);
+		} else if (l_data < 4096) {
+			fprintf(output_file,"0x0%x",l_data);
+		} else {
+			fprintf(output_file,"0x%x",l_data);
+		}
+		++l_ctr;
+		if (loop == l_datasize-1) {
+			fprintf(output_file, " ");
+		} else {
+			if (l_ctr == 10) {
+				l_ctr = 0;
+				fprintf(output_file, ",\n                    ");
+			} else {
+				fprintf(output_file, ", ");
+			}
+		}
+	}
+	
+	fseek(output_file,ftell(output_file)-1,0);
+	fprintf(output_file,"};\n\n");
+	
+	l_ctr = 0;
+	fprintf(output_file,"const u16 %sPalette[] = {\n                    ", refName);
+	for (loop = 0; loop < 256; ++loop) {
+		l_data = (pcx_image_palette[loop*3] | pcx_image_palette[(loop*3)+1]<<5 | pcx_image_palette[(loop*3)+2]<<10);
+		if (l_data == 00) {
+			fprintf(output_file,"0x0000");
+		} else if (l_data < 16) {
+			fprintf(output_file,"0x000%x",l_data);
+		} else if (l_data < 256) {
+			fprintf(output_file,"0x00%x",l_data);
+		} else if (l_data < 4096) {
+			fprintf(output_file,"0x0%x",l_data);
+		} else {
+			fprintf(output_file,"0x%x",l_data);
+		}
+		++l_ctr;
+		if (loop == 255) {
+			fprintf(output_file, " ");
+		} else {
+			if (l_ctr == 10) {
+				l_ctr = 0;
+				fprintf(output_file, ",\n                    ");
+			} else {
+				fprintf(output_file, ", ");
+			}
+		}
+	}
+	
+	fseek(output_file,ftell(output_file)-1,0);
+	fprintf(output_file,"};\n");
+	
 /*****************************************************************************/
 /* Terminate Program                                                         */
 /*****************************************************************************/
-
-   fclose(output_file);
-   free(input_file_buffer);
-
-   printf("Successfully created file %s\n\n",argv[2]);
-
-   return (0);
+	
+	fclose(output_file);
+	free(input_file_buffer);
+	
+	printf("Successfully created file %s\n\n", outfile);
+	
+	return (0);
 }
+
